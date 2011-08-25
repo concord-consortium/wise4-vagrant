dba8820b6dcdf7630b29b5b3da05c4b5de00c7b9
Stephen Bannasch <stephen.bannasch@gmail.com>
2011-08-25 15:17:34 -0400

    include reset_wise4_databases.rb in /home/vagrant

--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -4,3 +4,10 @@ template "/var/lib/tomcat6/webapps/vlewrapper/vle/node/setupNodes.js" do
   group "tomcat6"
   mode "0644"
 end
+
+cookbook_file "/home/vagrant/reset_wise4_databases.rb" do
+  source "reset_wise4_databases.rb"
+  owner "vagrant"
+  group "vagrant"
+  mode "0755"
+end
