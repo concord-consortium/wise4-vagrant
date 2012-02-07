5e3506765af33a733189ed7155b9bec1ce6ac088
Noah Paessel <knowuh@gmail.com>
2012-02-07 12:55:16 -0500

    Only create vagrants home if its not there.

--- rebuild/cookbooks/vagrant_main/recipes/default.rb
+++ rebuild/cookbooks/vagrant_main/recipes/default.rb
@@ -6,10 +6,17 @@ user "vagrant" do
   comment "vagrant user for vagrant/ec2 deploy convergence."
   shell "/bin/bash"
   home "/home/vagrant/"
-  supports :manage_home => true
+  # supports :manage_home => false
 end
 
-
+# create vagrants home directory unless it exists already:
+directory "/home/vagrant" do
+   mode 0775
+   owner "vagrant"
+   group "vagrant"
+   action :create
+   recursive true
+end
 
 
 # Item 1 
